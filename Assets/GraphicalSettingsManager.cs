bf84ae0efac14be6c6beadde4e3ff49748151873
Ismet Baran Surucu <baransrc@gmail.com>
2020-08-19 18:42:19 +0300

    Added Obstacle Circular Motion and FPS displayer.

---
+++ Assets/GraphicalSettingsManager.cs
@@ -0,0 +1,129 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GraphicalSettingsManager : MonoBehaviour
+{
+    [SerializeField] private bool _overrideResolution = false;
+    [SerializeField] private int _mobileFrameRate = 60;
+    [SerializeField] private int _webFrameRate = 60;
+    [SerializeField] private int _pcFrameRate = 60;
+    [SerializeField] private int _editorFrameRate = 60;
+    [SerializeField] private Vector2Int _mobileResolution = new Vector2Int(1280, 720);
+    [SerializeField] private Vector2Int _webResolution = new Vector2Int(1920, 1080);
+    [SerializeField] private Vector2Int _pcResolution = new Vector2Int(1920, 1080);
+    [SerializeField] private Vector2Int _editorResolution = new Vector2Int(1920, 1080);
+
+    private void Awake()
+    {
+        FixGraphicalSettings();
+    }
+
+    private Vector2Int GetAspectRatio()
+    {
+        var height = Camera.main.pixelHeight;
+        var width = Camera.main.pixelWidth;
+        var ratio = (Mathf.CeilToInt(100f * (float)width / (float)height));
+        var aspectRatio = new Vector2Int(16, 9);
+
+        switch (ratio)
+        {
+            case 126:
+            case 125:
+            case 124: // 5:4
+                aspectRatio.x = 5;
+                aspectRatio.y = 4;
+                break;
+
+            case 134:
+            case 133:
+            case 132: // 4:3
+                aspectRatio.x = 4;
+                aspectRatio.y = 3;
+                break;
+
+            case 151:
+            case 150:
+            case 149: // 3:2
+                aspectRatio.x = 3;
+                aspectRatio.y = 2;
+                break;
+
+            case 162:
+            case 161:
+            case 160: // 16:10
+                aspectRatio.x = 16;
+                aspectRatio.y = 10;
+                break;
+
+            case 167:
+            case 166:
+            case 165: // 5:3
+                aspectRatio.x = 5;
+                aspectRatio.y = 3;
+                break;
+
+            case 179:
+            case 178:
+            case 177: // 16:9
+                aspectRatio.x = 16;
+                aspectRatio.y = 9;
+                break;
+
+            case 201:
+            case 200:
+            case 199: // 18:9
+                aspectRatio.x = 18;
+                aspectRatio.y = 9;
+                break;
+
+            case 218:
+            case 217:
+            case 216: // 13:6
+                aspectRatio.x = 13;
+                aspectRatio.y = 6;
+                break;
+        }
+
+        return aspectRatio;
+    }
+
+    private void FixGraphicalSettings()
+    {
+        var frameRate = 60;
+        var resolution = new Vector2Int(1920, 1080);
+        var aspectRatio = GetAspectRatio();
+        Debug.Log(aspectRatio);
+        var multiplier = resolution.x / aspectRatio.x;
+
+#if UNITY_ANDROID || UNITY_IOS
+        frameRate = _mobileFrameRate;
+        multiplier = _mobileResolution.x / aspectRatio.x;
+        resolution = multiplier * aspectRatio;
+#endif
+
+#if UNITY_EDITOR
+        frameRate = _editorFrameRate;
+        multiplier = _editorResolution.x / aspectRatio.x;
+        resolution = multiplier * aspectRatio;
+#endif
+
+#if UNITY_WEBGL
+        frameRate = _webFrameRate;
+        multiplier = _webResolution.x / aspectRatio.x;
+        resolution = multiplier * aspectRatio;
+#endif
+
+#if UNITY_STANDALONE
+        frameRate = _pcFrameRate;
+        multiplier = _pcResolution.x / aspectRatio.x;
+        resolution = multiplier * aspectRatio;
+#endif
+
+        QualitySettings.vSyncCount = 0;
+        Application.targetFrameRate = frameRate;
+        if (!_overrideResolution) return;
+
+        Screen.SetResolution(resolution.x, resolution.y, true);
+    }
+}
---
+++ Assets/ObstacleCircularMotion.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum CircularMovementAxis
+{
+    X,
+    Y,
+    Z
+}
+
+[RequireComponent(typeof(ObstacleCollision))]
+[RequireComponent(typeof(Rigidbody))]
+public class ObstacleCircularMotion : MonoBehaviour
+{
+    [SerializeField] private CircularMovementAxis _movementAxis;
+    [SerializeField] private float _radiusWidth;
+    [SerializeField] private float _radiusHeight;
+    [SerializeField] private float _durationOfOneTurn;
+    [SerializeField] private bool _reverseMode;
+
+    private ObstacleCollision _obstacleCollision;
+    private Rigidbody _rigidbody;
+    private static float _twoTimesPi = Mathf.PI * 2;
+    private bool _shouldMove;
+
+    private void Awake()
+    {
+        _obstacleCollision = GetComponent<ObstacleCollision>();
+        _rigidbody = GetComponent<Rigidbody>();
+        _shouldMove = true;
+
+        _obstacleCollision.OnCollisionOccured += StopMotion;
+
+        StartCoroutine(CircularMovement());
+    }
+
+    private void StopMotion()
+    {
+        _shouldMove = false;
+    }
+
+    private IEnumerator CircularMovement()
+    {
+        var durationOfOneTurnInverse = 1f / _durationOfOneTurn;
+        var initialPosition = _rigidbody.position;
+
+        while (_shouldMove)
+        {
+            var step = 0f;
+            var angle = 0f;
+
+            while (step < 1f)
+            {
+                if (!_shouldMove)
+                {
+                    break;
+                }
+
+                step += Time.deltaTime * durationOfOneTurnInverse;
+                angle = step * _twoTimesPi;
+                angle = _reverseMode ? -angle : angle;
+
+                var x = 0f;
+                var y = 0f;
+                var z = 0f;
+
+                switch (_movementAxis)
+                {
+                    case CircularMovementAxis.X:
+                        z += Mathf.Cos(angle) * _radiusWidth;
+                        y += Mathf.Sin(angle) * _radiusHeight;
+                        break;
+                    case CircularMovementAxis.Y:
+                        x += Mathf.Cos(angle) * _radiusWidth;
+                        z += Mathf.Sin(angle) * _radiusHeight;
+                        break;
+                    case CircularMovementAxis.Z:
+                        x += Mathf.Cos(angle) * _radiusWidth;
+                        y += Mathf.Sin(angle) * _radiusHeight;
+                        break;
+                }
+
+                _rigidbody.position = initialPosition + new Vector3(x, y, z);
+
+                yield return null;
+            }
+
+        }
+    }
+
+    private void OnDestroy()
+    {
+        _obstacleCollision.OnCollisionOccured -= StopMotion;
+    }
+}
