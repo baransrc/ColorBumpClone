dc7d04d27c98f64b957d3e8bf456915b98f0f97e
Ismet Baran Surucu <baransrc@gmail.com>
2020-08-20 05:04:06 +0300

    Added level finish script.

---
+++ Assets/FinishLevelManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class FinishLevelManager : MonoBehaviour
+{
+    [SerializeField] private float _slowMotionDuration;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            StartCoroutine(FinishLevel());
+        }
+    }
+
+    private IEnumerator FinishLevel()
+    {
+        Time.timeScale = 0.5f;
+
+        yield return new WaitForSeconds(_slowMotionDuration);
+
+        Debug.Log("Level Ended!");
+    }
+}
